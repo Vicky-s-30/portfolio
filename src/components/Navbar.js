f76759dd45274208739ca2e87fa8e69ebce12302
vicky <johndoe@email.com>
2024-04-01 19:28:45 +0530

    navbar link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,8 +39,10 @@ function NavBar() {
       className={navColour ? "sticky" : "navbar"}
     >
       <Container>
-        <Navbar.Brand href="/" className="d-flex">
+        <Navbar.Brand className="d-flex">
+        <Nav.Link as={Link} to="/" className="m-0 p-0">
           <img src={logo} className="img-fluid logo" alt="brand" />
+          </Nav.Link>
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
